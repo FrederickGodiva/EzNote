873eed433f5a989cf383e7b875bf32d44e967d4d
DP5-Frederick-038 <godivafrederick@gmail.com>
2023-12-20 22:40:31 +0700

    Front End Bug

---
+++ public/js/notes.js
@@ -0,0 +1,41 @@
+const addBtn = document.querySelector(".add");
+const notes = document.querySelector(".notes");
+
+addBtn.addEventListener("click", () => {
+  const newNote = document.createElement("div");
+  newNote.classList.add("note");
+  newNote.innerHTML = `
+    <textarea placeholder="Title" class="title-area"></textarea>
+    <textarea placeholder="Start writing" class="note-area"></textarea>
+  `;
+  notes.appendChild(newNote);
+
+  noteFocus(newNote);
+});
+
+function noteFocus(note) {
+  const inputFields = note.getElementsByTagName("textarea");
+  let actionBtn;
+
+  const removeActionBtn = () => {
+    if (actionBtn) {
+      note.removeChild(actionBtn);
+      actionBtn = null;
+    }
+  };
+
+  for (let i = 0; i < inputFields.length; i++) {
+    inputFields[i].addEventListener("focus", () => {
+      if (!actionBtn) {
+        actionBtn = document.createElement("div");
+        actionBtn.classList.add("d-flex", "justify-content-end", "gap-3");
+        actionBtn.innerHTML = `
+          <button class="btn btn-success save">Save</button>
+          <button class="btn btn-danger delete">Delete</button>`;
+        note.appendChild(actionBtn);
+      }
+    });
+
+    inputFields[i].addEventListener("blur", removeActionBtn);
+  }
+}
