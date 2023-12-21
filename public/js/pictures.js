9b6c93f59bfe1c5bd0b4846acffbb82a735c8339
DP5-Frederick-038 <godivafrederick@gmail.com>
2023-12-21 21:32:40 +0700

    Front End Done

--- public/js/pictures.js
+++ public/js/pictures.js
@@ -1,3 +1,15 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      input.nextElementSibling.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
 const addBtn = document.querySelector(".add");
 const uploadImg = document.querySelector(".upload-img");
 
@@ -6,9 +18,22 @@ addBtn.addEventListener("click", () => {
   newPicture.classList.add("picture");
 
   newPicture.innerHTML = `
-    <input type="file" />
-    <button type="submit" class="upload">Upload</button>
+      <input type="file" class="imgInp" />
+      <img src="#" alt="Preview" class="preview" style="display: none;" />
+      <button type="submit" class="upload">Upload</button>
   `;
 
   uploadImg.appendChild(newPicture);
+
+  const inputFile = newPicture.querySelector(".imgInp");
+  const imgPreview = newPicture.querySelector("img");
+
+  inputFile.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+      imgPreview.style.display = "block";
+      readURL(this);
+    } else {
+      imgPreview.style.display = "none";
+    }
+  });
 });
